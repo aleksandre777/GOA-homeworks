932bcc507359364393c56a862004947fedabeabf
aleksandre777 <aleksandrecarcidze@gmail.com>
2025-07-14 10:47:21 +0400

    second commit

---
+++ level03/Class Work/classwork.py
@@ -0,0 +1,10 @@
+city = "paris "
+print(city) 
+city = "munich"
+print(city)
+
+Country = "france" 
+contury = "germany"
+COUNTRy = "Georgia"
+CouNtry = "belgium"
+COUNtry = "israel" 
---
+++ level03/Homework/Homework.py
@@ -0,0 +1,52 @@
+#  დავალება 1 
+
+num_1 = 98
+num_2 = 62
+sum_of_numbers = num_1 + num_2 
+sum_of_numbers = num_1 * num_2
+sum_of_numbers = num_1 / num_2 
+sum_of_numbers = num_1 - num_2 
+
+# დავალება 2 
+
+name = "goga"
+name = "nika"
+name = "tengo"
+name = "giorgi"
+name = "aleksandre" 
+print(name) 
+
+city = "manchester"
+City = "munich"
+CIty = "rio de janeiro"
+CITy = "tbilisi"
+CITY = "las vegas"
+print(city)
+print(City) 
+print(CIty)
+print(CITy)
+print(CITY)
+
+# დავალება 3
+
+#2name = "giorgi"     -      ცვლადის სახელი არ შეიძლება იწყენოდეს რიცხვით
+#user{name = "bubunauri"    -   snake case ის დროს ვიყენებთ underscore -s _
+#user_name = goga       -      სახელი უნდა იყოს ბრჭყალებში
+#user-surname = axalaia    -  აქაც უნდა გამოვიყენოთ undersocre და სახელი ჩავსვათ ბრჭყალებში 
+
+# დავალება 4 
+user_name1 = "goga"
+user_name2 = "tengo"
+user_name3 = "nika"
+user_name4 = "giorgi"
+user_name5 = "aleksandre"
+print(user_name1+user_name2+user_name3+user_name4+user_name5)
+
+# დავალება 5
+
+name = "aleksandre"
+number = 10 
+print(name*number)  
+
+# დავალება 6 
+print("aleksandre"+20)
