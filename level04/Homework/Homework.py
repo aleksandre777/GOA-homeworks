ea32b9b797001d729a97ad981367017800787e77
aleksandre777 <aleksandrecarcidze@gmail.com>
2025-07-18 17:14:00 +0400

    second commit

---
+++ level04/Homework/Homework.py
@@ -0,0 +1,48 @@
+# input - არის მნიშვნელობა რომლის შემოტანა შეგვიძლია და შემდეგ მისი დამუშვაება
+# output - ვღებულობთ შემოტანილი ინფორმაციაზე პასუხს და გამოაქვს ტერმინალზე
+
+name = input("aleksandre")
+print(type(name))
+
+my_name = "aleksandre" 
+favourite_city = "barcelona"
+favourite_team = "barcelona"
+best_carbrend = "BMW"
+football_academy = "inter"          # აქ მყოფი ყველა მონაცემთა ტიპი string ია 
+
+
+my_height = 163
+mouse_size = 15
+my_age = 13 
+its_year = 2025 
+next_year = 2026               # აქ ყველა მონაცემტა ტიპი integer ია  
+
+
+plane_ticket= 150.50 
+bank_balance = 10.80 
+movie_lenght = 1.30
+my_weight = 50.25              # აქ ყველა მონაცემთა ტიპი float ია
+
+
+school = "Greenschool"
+height = 163 
+plane_ticket = 250.12 
+print(type(school))
+print(type(height))
+print(type(plane_ticket))
+
+
+team = input("chelsea")
+tournamet = input("clubworldcup")
+print(team+tournamet)
+
+
+num1=(90) 
+num2=(60)
+num3=(10)
+num4=(70)
+num5=(52) 
+print(int(num1+num2+num3+num4+num5)/5) 
+
+
+
---
+++ level05/Classwork/classwork.py
@@ -0,0 +1,4 @@
+num1=input(90)
+num2=input(50)
+print(90>50) 
+print(90==50)
