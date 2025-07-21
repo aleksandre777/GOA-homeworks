10dd611dd6ffb10b41f017bfc0db67cc0bbbd2d1
aleksandre777 <aleksandrecarcidze@gmail.com>
2025-07-21 10:52:44 +0400

    second commit

---
+++ level06/classwork/classwork.py
@@ -0,0 +1,16 @@
+# logical operations იყოფა ორ ნაწილად and , or 
+# and ი ანიჭებს უპრიატესობას false მაგ: 
+print(True and False) 
+# or უპრიატესობას ანიჭებს true ს მაგ: 
+print(True or False) 
+
+
+input("nika")
+input("saba")
+print("nika"=="saba") 
+
+print(False and False) 
+print(True and False) 
+
+print(True or True) 
+print(False or True)
